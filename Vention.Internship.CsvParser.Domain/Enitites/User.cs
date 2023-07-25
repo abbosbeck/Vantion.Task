71a66a7436e8e699ec7be3bc80cad319e1a9fd0f
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-07-25 14:10:35 +0500

    DOMAIN: Create User Entity

---
+++ Vention.Internship.CsvParser.Domain/Enitites/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vention.Internship.CsvParser.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserIdentifier { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
